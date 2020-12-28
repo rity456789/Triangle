40969719230930045e022c36c8e9586c0b7035c0
phbinh <rity456789@gmail.com>
2020-12-28 23:57:33 +0700

    write test equilateral triangle

--- Triangle.test.js
+++ Triangle.test.js
@@ -27,3 +27,11 @@ test("should specify not a triangle because edges are on one line", () => {
   const triangle = new Triangle(p1, p2, p3);
   expect(triangle.getType()).toBe(triangleType.NOT_A_TRIANGLE);
 });
+
+test("should specify equilateral triangle", () => {
+  const p1 = new Point(0, 0);
+  const p2 = new Point(2, 0);
+  const p3 = new Point(1, 1.732);
+  const triangle = new Triangle(p1, p2, p3);
+  expect(triangle.getType()).toBe(triangleType.EQUILATERAL);
+});
